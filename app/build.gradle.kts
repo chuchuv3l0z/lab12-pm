702aa4b2324197f976144e5fc3a97129654d68b1
Thomas <chuchuv3l0z@gmail.com>
2025-11-09 18:50:22 -0500

    feat: agregar dependencias de Google Maps Compose (maps, utils y widgets)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // Google Maps Compose
+    val mapsComposeVersion = "4.4.1"
+
+    implementation("com.google.maps.android:maps-compose:$mapsComposeVersion")
+
+    // Utilidades de Google Maps para Jetpack Compose
+    implementation("com.google.maps.android:maps-compose-utils:$mapsComposeVersion")
+
+    // Widgets de Google Maps Compose
+    implementation("com.google.maps.android:maps-compose-widgets:$mapsComposeVersion")
+
 }
