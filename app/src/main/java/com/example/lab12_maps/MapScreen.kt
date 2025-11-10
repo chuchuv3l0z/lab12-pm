26178f7e939276d59aa0c490f659e8b5968aac80
Thomas <chuchuv3l0z@gmail.com>
2025-11-09 19:56:35 -0500

    feat: controlar cámara con animación hacia Yura

--- app/src/main/java/com/example/lab12_maps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_maps/MapScreen.kt
@@ -3,7 +3,9 @@ package com.example.lab12_maps
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -15,11 +17,21 @@ import com.google.maps.android.compose.rememberMarkerState
 @Composable
 fun MapScreen() {
     val arequipaLocation = LatLng(-16.4040102, -71.559611)
+    val yuraLocation = LatLng(-16.2520984, -71.6836503)
+
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(arequipaLocation, 12f)
     }
 
-    // Lista de ubicaciones adicionales
+    // Animar movimiento de cámara hacia Yura
+    LaunchedEffect(Unit) {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(yuraLocation, 12f),
+            durationMs = 3000
+        )
+    }
+
+    // Lista de ubicaciones extra
     val locations = listOf(
         LatLng(-16.433415, -71.5442652), // JLByR
         LatLng(-16.4205151, -71.4945209), // Paucarpata
@@ -31,7 +43,7 @@ fun MapScreen() {
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Marcador principal (Arequipa)
+            // Marcador principal
             Marker(
                 state = rememberMarkerState(position = arequipaLocation),
                 icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE),
@@ -46,6 +58,13 @@ fun MapScreen() {
                     snippet = "Punto de interés"
                 )
             }
+
+            // Marcador de Yura
+            Marker(
+                state = rememberMarkerState(position = yuraLocation),
+                title = "Yura",
+                snippet = "Destino de la animación"
+            )
         }
     }
 }
